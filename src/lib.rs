6986330ea990d887c6fd1f767b8c52a713e0906f
teln0 <telnobynoyator@gmail.com>
2021-03-27 15:50:43 +0100

    Added doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use std::sync::atomic::{AtomicU64, Ordering};
 
 static CURRENT_ID: AtomicU64 = AtomicU64::new(0);
 
+/// Increments and returns the current ID, effectively creating a unique ID at compile time
+/// ever time this function is called
 #[proc_macro_hack]
 pub fn const_id(_input: TokenStream) -> TokenStream {
     let expr = CURRENT_ID.fetch_add(1, Ordering::SeqCst);
